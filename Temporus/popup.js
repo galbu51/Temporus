7a55ca6069cfaf0cfabaab74785c7d25adf8e846
galbu51 <guialbuquerque.ramos@gmail.com>
2026-07-14 12:18:55 -0300

    Add files via upload
    # 14.07.26 - FAB LAB
    Foi criada uma extensão.

---
+++ Temporus/popup.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const botaoModoEscuro = document.getElementById("darkMode");
+
+
+    botaoModoEscuro.addEventListener("click", () => {
+
+        alert("Botão funcionando!");
+
+    });
+
+});
